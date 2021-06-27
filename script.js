690d115e417a18cf6d38d535c6cb05f4649cc940
MihirSharma64 <mihirsharma64@gmail.com>
2021-06-28 00:59:22 +0530

    Changed colors

--- script.js
+++ script.js
@@ -1,49 +1,58 @@
 // console.log('Hello');
 let input = new Array(9);
 let solutionarr = new Array(9);
+let vis = new Array(9);
 for (let i = 0; i < 9; i++) {
 	input[i] = new Array(9);
 	solutionarr[i] = new Array(9);
+	vis[i] = new Array(9);
 }
 
 for (let i = 0; i < 9; i++) {
 	for (let j = 0; j < 9; j++) {
 		input[i][j] = 0;
 		solutionarr[i][j] = 0;
+		vis[i][j] = 0;
 	}
 }
+
+
 let boxes = document.querySelectorAll('.sudoku-container input');
 let boxes2 = document.querySelectorAll('.visualizer-container input');
-
 let solvebtn = document.querySelector('.solve');
 let resetbtn = document.querySelector('.reset');
+
+
 for (let i = 0; i < boxes.length; i++) {
 	boxes[i].addEventListener('change', handleValueChange);
 }
 
 function handleValueChange(e) {
-	flag = true;
 	let targetID = e.target.id;
 	let cellNo = Number(targetID);
 	let row = Math.floor(cellNo / 9);
 	let col = Math.floor(cellNo % 9);
-	console.log(row + ' ' + col);
 	let val = Number(e.target.value);
 	input[row][col] = val;
 	solutionarr[row][col] = val;
+	vis[row][col] = 1;
+	boxes2[cellNo].value = val;
 }
 
 solvebtn.addEventListener('click', solve);
 
 function solve() {
-	// func(input, 0, 0);
 	func(solutionarr, 0, 0);
 
 	for (let i = 0; i < 9; i++) {
 		for (let j = 0; j < 9; j++) {
 			let cellNo = i * 9 + j;
-			// let box = document.querySelector(`#${cellNo}`);
+			if(vis[i][j] != 1){
+				let classname = boxes[cellNo].className;
+				boxes[cellNo].className = classname + " green";
+			}
 			boxes[cellNo].value = solutionarr[i][j];
+
 		}
 	}
 }
@@ -117,14 +126,13 @@ resetbtn.addEventListener('click', function () {
 	for (let i = 0; i < 9; i++) {
 		for (let j = 0; j < 9; j++) {
 			solutionarr[i][j] = 0;
+			input[i][j] = 0;
 		}
 	}
 
 	for (let i = 0; i < 9; i++) {
 		for (let j = 0; j < 9; j++) {
 			let cellNo = i * 9 + j;
-			// let box = document.querySelector(`#${cellNo}`);
-			console.log(cellNo);
 			boxes[cellNo].value = '';
 		}
 	}
@@ -134,8 +142,8 @@ let visualizerbtn = document.querySelector('.visualize');
 let delay = 0;
 let visualize = async () => {
 	delay = document.querySelector(".delay").value;
-	console.log('HI');
-	await func2(input, 0, 0);
+	let board = [...input];
+	await func2(board, 0, 0);
 };
 visualizerbtn.addEventListener('click', visualize);
 
@@ -163,9 +171,6 @@ let func2 = async (board, row, col) => {
 			await updateVal(cellNo,i);
 			if (canBePlaced2(board, row, col, i)) {
 				board[row][col] = i;
-
-				// boxes2[cellNo].value = i;
-				// await updateVal(cellNo, i);
 				tempans = await func2(board, nextrow, nextcol);
 				if (tempans == true) {
 					return true;
@@ -185,6 +190,12 @@ const updateVal = async (cellNo, val) => {
 			if (val == 0) {
 				boxes2[cellNo].value = '';
 			} else {
+				let row = Math.floor(cellNo/9)
+				let col = Math.floor(cellNo%9)
+				if(vis[row][col] != 1){
+					let classname = boxes2[cellNo].className;
+					boxes2[cellNo].className = classname + " green";
+				}
 				boxes2[cellNo].value = val;
 			}
 			resolve();
